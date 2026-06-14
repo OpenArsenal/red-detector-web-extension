4c276a24a9780e4c8f658b9b464a67517c09d0db
Okiki Ojo <okiki@thunderstrike.co>
2026-06-13 22:48:42 -0400

    refactor(detection): move SOURCE_LIMITS from ./dectections/rules.ts to ./detections/rules/index.ts and update rules.ts to export from index
    Signed-off-by: Okiki Ojo <okiki@thunderstrike.co>

--- src/lib/detection/rules.ts
+++ src/lib/detection/rules.ts
@@ -1,21 +1,7 @@
 import type { TechnologyDefinition } from './types';
 import { generatedTechnologyDefinitions, technologyDefinitionsByCategory } from './rules/index';
 
-export const SOURCE_LIMITS = {
-	htmlChars: 200_000,
-	scriptSrc: 300,
-	stylesheetHref: 300,
-	resourceUrls: 500,
-	requestUrls: 500,
-	linkTags: 200,
-	storageKeys: 200,
-	scriptContentChars: 40_000,
-	stylesheetContentChars: 40_000,
-	metaValueChars: 500,
-	metaValuesPerKey: 5,
-	cookieNames: 200,
-	evidenceValueChars: 160,
-} as const;
+export { SOURCE_LIMITS } from './rules/index';
 
 export const technologyDefinitions = generatedTechnologyDefinitions satisfies TechnologyDefinition[];
 export const technologiesByCategory = technologyDefinitionsByCategory;
--- src/lib/detection/rules/index.ts
+++ src/lib/detection/rules/index.ts
@@ -51,6 +51,33 @@ import { widgetsMiscTechnologyDefinitions } from './widgets-misc';
 import { domainParkingTechnologyDefinitions } from './domain-parking';
 import { fundraisingDonationsTechnologyDefinitions } from './fundraising-donations';
 
+export const SOURCE_LIMITS = {
+	htmlChars: 200_000,
+	textChars: 80_000,
+	scriptSrc: 300,
+	stylesheetHref: 300,
+	resourceUrls: 500,
+	requestUrls: 500,
+	linkTags: 200,
+	storageKeys: 200,
+	scriptContentItems: 200,
+	stylesheetContentItems: 200,
+	scriptContentChars: 40_000,
+	stylesheetContentChars: 40_000,
+	scriptContentTotalChars: 320_000,
+	stylesheetContentTotalChars: 240_000,
+	jsGlobals: 6_000,
+	jsGlobalValueChars: 500,
+	robotsTxtChars: 40_000,
+	headers: 80,
+	headerValueChars: 1_000,
+	probeResults: 50,
+	metaValueChars: 500,
+	metaValuesPerKey: 5,
+	cookieNames: 200,
+	evidenceValueChars: 160,
+} as const;
+
 export const technologyDefinitionsByCategory = {
 	"framework": frameworkTechnologyDefinitions,
 	"ui-library": uiLibraryTechnologyDefinitions,
