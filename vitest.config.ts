f4b6bc9d7f6b072bc286b543713fa3a7603f09f0
Okiki Ojo <okiki@thunderstrike.co>
2026-06-23 12:03:49 -0400

    refactor: enhance vitest configuration with path aliasing support
    Signed-off-by: Okiki Ojo <okiki@thunderstrike.co>

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vitest/config';
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
+	resolve: {
+		tsconfigPaths: true,
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+			'~': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
 	test: {
 		globals: true,
 	},
